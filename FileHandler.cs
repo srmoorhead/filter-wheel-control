35dc4d5955c6ecde65540a2fdf7157ce6f4c07fc
Sean <smoo93@gmail.com>
2016-07-19 10:23:43 -0500

    File Handling Update
    FileHandling now uses the built-in save function instead of creating a new IImageDataSet.  Much more elegant file export process.

--- FileHandler.cs
+++ FileHandler.cs
@@ -6,6 +6,8 @@ using PrincetonInstruments.LightField.AddIns;
 using System.Windows;
 using System.IO;
 using System.Threading;
+using System.Xml; // for parsing the .spe footer
+using nom.tam.fits; // for working with the .fits file
 
 namespace FilterWheelControl
 {
@@ -17,13 +19,13 @@ namespace FilterWheelControl
         private static readonly int FILENAME_INDEX = 1;
         private static readonly string FITS_EXTENSION = ".fits"; // the file extension for a fits file
         private static readonly string SPE_EXTENSION = ".spe"; // the file extension for an spe file
-
+        
         #endregion // Static Variables
 
         #region Instance Variables
 
-        IExperiment _exp;
-        IFileManager _file_mgr;
+        private IExperiment _exp;
+        private IFileManager _file_mgr;
 
         #endregion // Instance Variables
 
@@ -53,69 +55,48 @@ namespace FilterWheelControl
             if ((baseFile = CreateFileName(nameInfo, fnum, padSize)) == "ERROR")
                 return false;
 
-            // Save the frame to a temporary .spe file
-            ImageDataFormat format = (ImageDataFormat)_exp.GetValue(CameraSettings.AcquisitionPixelFormat);
-            RegionOfInterest[] regions = _exp.SelectedRegions;
-
-            string tempFileName = baseFile + SPE_EXTENSION;
-            BuildTempFile(format, regions, frame, tempFileName);
-            
+            // Save the current frame as an .spe file
+            _file_mgr.SaveFile(frame, baseFile + SPE_EXTENSION);
+             
             // Build export settings object
             IFitsExportSettings settings = GenerateExportSettings(nameInfo);
 
             // Validate the output to ensure no errors
-            return WriteFile(settings, tempFileName, _file_mgr);
+            return WriteFile(settings, baseFile);
         }
 
         /// <summary>
-        /// Validates the output and exports the file
+        /// Validates the output and exports the file.  Deletes the temporary .spe file
         /// </summary>
         /// <param name="settings">The IFitsExportSettings pertaining to this file</param>
         /// <param name="name">The name of the file after export</param>
         /// <param name="filemgr">The IFileManager object handling exporting</param>
         /// <returns>true if the export was a success, false otherwise</returns>
-        private bool WriteFile(IFitsExportSettings settings, string name, IFileManager filemgr)
+        private bool WriteFile(IFitsExportSettings settings, string name)
         {
-            if (ValidateOutput(settings, name))
+            if (ValidateOutput(settings, name + SPE_EXTENSION))
             {
                 // Export the file
-                filemgr.Export((IExportSettings)settings, name);
+                _file_mgr.Export((IExportSettings)settings, name + SPE_EXTENSION);
 
                 // Delete the temp file
                 try
                 {
-                    File.Delete(name);
+                    File.Delete(name + SPE_EXTENSION);
                 }
                 catch (IOException)
                 {
                     return false;
                 }
+                
                 return true;
             }
             else
                 return false;
         }
 
-        /// <summary>
-        /// Builds the IFitsExportSettings object.
-        /// IncludeAllExperimentInformation is set to true
-        /// OutputPathOption is set to ExportOutputPathOption.CustomPath
-        /// OutputMode is set to ExportOutputMode.OneFilePerRoiPerFrame
-        /// CustomOutputPath is set to the directory the user specified
-        /// </summary>
-        /// <param name="nameInfo">An array containing the directory and the base file name</param>
-        /// <param name="filemgr">The IFileManager object to use for exporting</param>
-        /// <returns>The IFitsExportSettings generated with the given input</returns>
-        private IFitsExportSettings GenerateExportSettings(string[] nameInfo)
-        {
-            IFitsExportSettings settings = (IFitsExportSettings)_file_mgr.CreateExportSettings(ExportFileType.Fits);
-            settings.IncludeAllExperimentInformation = true;
-            settings.CustomOutputPath = nameInfo[DIRECTORY_INDEX];
-            settings.OutputPathOption = ExportOutputPathOption.CustomPath;
-            settings.OutputMode = ExportOutputMode.OneFilePerRoiPerFrame;
-            return settings;
-        }
-
+        #region Generate File Name
+        
         /// <summary>
         /// Generate the filename for the exported file.
         /// </summary>
@@ -135,7 +116,7 @@ namespace FilterWheelControl
                 exportLoc = baseFile + append + FITS_EXTENSION;
                 edited = true;
             }
-            if (edited && fnum == 1)
+            if (edited && (fnum == 1))
             {
                 MessageBoxResult useNewName = MessageBox.Show("A file already exists with the name " + baseFile + FITS_EXTENSION + "\n\nYour data will be saved as: " + exportLoc + "\n\nIs this okay?", "Export Error - Pre-Existing File", MessageBoxButton.YesNo, MessageBoxImage.Warning);
                 if (useNewName == MessageBoxResult.No)
@@ -143,45 +124,6 @@ namespace FilterWheelControl
             }
             return baseFile + append;
         }
-
-        /// <summary>
-        /// Saves one frame of data to an .spe file with filename name
-        /// </summary>
-        /// <param name="app">The current LightField Application</param>
-        /// <param name="filemgr">The FileManager associated with the LightField Application</param>
-        /// <param name="frame">The frame to be saved.</param>
-        /// <param name="name">The name of the file to save.</param>
-        private void BuildTempFile(ImageDataFormat format, RegionOfInterest[] regions, IImageDataSet frame, string name)
-        {
-            if (File.Exists(name))
-                File.Delete(name);
-
-            IImageDataSet data = _file_mgr.CreateFile(name, regions, 1, format);
-
-            // Copy the data over to the file
-            int numROIs = regions.Count();
-            IImageData[] split_frame = SeparateROIs(frame, numROIs, 0);
-            IImageData[] split_file = SeparateROIs(data, numROIs, 0);
-            for (int i = 0; i < numROIs; i++)
-                split_file[i].SetData(split_frame[i].GetData());
-        }
-
-        /// <summary>
-        /// Takes an IImageDataSet object and separates it into an array of IImageData objects by region of interest.
-        /// </summary>
-        /// <param name="exposure">An IImageDataSet object</param>
-        /// <param name="num_rois">The number of regions of interest in one frame of the IImageDataSet object</param>
-        /// <param name="frame">The frame number to separate by region of interest</param>
-        /// <returns>IImageData[] with each element a different region of interest from the same frame</returns>
-        private IImageData[] SeparateROIs(IImageDataSet exposure, int num_rois, int frame)
-        {
-            IImageData[] separated = new IImageData[num_rois];
-            for (int i = 0; i < num_rois; i++)
-                separated[i] = exposure.GetFrame(i, frame);
-            return separated;
-        }
-
-        #region Generate File Name
 
         /// <summary>
         /// Returns the string representing the user-specified file name as entered in the LightField settings pane.
@@ -268,6 +210,26 @@ namespace FilterWheelControl
         #endregion // Generate File Name
 
         #region Validate Fits Export
+
+        /// <summary>
+        /// Builds the IFitsExportSettings object.
+        /// IncludeAllExperimentInformation is set to true
+        /// OutputPathOption is set to ExportOutputPathOption.CustomPath
+        /// OutputMode is set to ExportOutputMode.OneFilePerRoiPerFrame
+        /// CustomOutputPath is set to the directory the user specified
+        /// </summary>
+        /// <param name="nameInfo">An array containing the directory and the base file name</param>
+        /// <param name="filemgr">The IFileManager object to use for exporting</param>
+        /// <returns>The IFitsExportSettings generated with the given input</returns>
+        private IFitsExportSettings GenerateExportSettings(string[] nameInfo)
+        {
+            IFitsExportSettings settings = (IFitsExportSettings)_file_mgr.CreateExportSettings(ExportFileType.Fits);
+            settings.IncludeAllExperimentInformation = true;
+            settings.CustomOutputPath = nameInfo[DIRECTORY_INDEX];
+            settings.OutputPathOption = ExportOutputPathOption.CustomPath;
+            settings.OutputMode = ExportOutputMode.OneFilePerRoiPerFrame;
+            return settings;
+        }
 
         /// <summary>
         /// Validates the settings of an export and handles any errors
